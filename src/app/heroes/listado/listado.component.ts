ccdc65032a92502331c4de132ab225e91453e0eb
Kevin Ulloa <kvnulloa.23@gmail.com>
2021-07-16 21:38:42 -0500

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+
+})
+export class ListadoComponent {
+
+  heroes: string[] = ["Spiderman", "Ironman", "Hulk", "Thor","GreenLight"];
+
+  heroeBorrado: string = "";
+
+
+  borrarHeroe(){
+
+   this.heroeBorrado = this.heroes.shift() || " ";
+
+
+
+  }
+
+
+}
